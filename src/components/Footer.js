e7088a61773e39bd5a51ab7e7401de13784c98be
Google Jr <andilembele020@gmail.com>
2021-02-25 15:52:10 +0200

    Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="d-flex flex-column">
+      <footer className="footer">
+        <div>
+          <a href="https://github.com/xeroxzen">Andile Jaden Mbele</a>
+          <span>&copy; {new Date().getUTCFullYear()} EffectivaX</span>
+        </div>
+      </footer>
+    </div>
+  );
+}
